cf1f154da57676e20f61f9cd1d60bfaa15851b4e
joaoviniciussant <161514691+joaoviniciussant@users.noreply.github.com>
2024-11-13 17:07:51 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,18 @@
+criaCartao{
+    'Programação',
+    'O queé JavaScript?',
+    'O JavaScript é uma linguagem de programação'
+}
+
+criaCartao{
+    'Geografia',
+    'Qual a capital da França',
+    'A capital da França é Paris'
+}
+
+criaCartao{
+    categoria,
+    pergunta,
+    resposta
+}
+
